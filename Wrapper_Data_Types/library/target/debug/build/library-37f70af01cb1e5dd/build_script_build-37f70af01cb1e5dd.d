32b2250facbd76646ce85d12272beb02029a51fa
Steve McCown <steven.mccown@gmail.com>
2021-09-08 14:32:03 -0600

    Delete Wrapper_Data_Types/library/target directory
    Removing debug info

--- Wrapper_Data_Types/library/target/debug/build/library-37f70af01cb1e5dd/build_script_build-37f70af01cb1e5dd.d
+++
@@ -1,5 +0,0 @@
-/Users/mccown/Development/Anonyome/ffi-tutorials/Wrapper_Data_Types/library/target/debug/build/library-37f70af01cb1e5dd/build_script_build-37f70af01cb1e5dd: build.rs
-
-/Users/mccown/Development/Anonyome/ffi-tutorials/Wrapper_Data_Types/library/target/debug/build/library-37f70af01cb1e5dd/build_script_build-37f70af01cb1e5dd.d: build.rs
-
-build.rs:
--- Wrapper_Data_Types/library/target/debug/build/serde-e80d045b00f838da/build_script_build-e80d045b00f838da.d
+++
@@ -1,5 +0,0 @@
-/Users/mccown/Development/Anonyome/ffi-tutorials/Wrapper_Data_Types/library/target/debug/build/serde-e80d045b00f838da/build_script_build-e80d045b00f838da: /Users/mccown/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.130/build.rs
-
-/Users/mccown/Development/Anonyome/ffi-tutorials/Wrapper_Data_Types/library/target/debug/build/serde-e80d045b00f838da/build_script_build-e80d045b00f838da.d: /Users/mccown/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.130/build.rs
-
-/Users/mccown/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.130/build.rs:
